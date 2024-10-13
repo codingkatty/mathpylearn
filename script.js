40426417612e63e0b04cf5c7d9892db5a6252b55
mohit-1710 <megamohit2006@gmail.com>
2024-10-13 23:23:28 +0530

    added hamburger menu icon for mobile devices

--- script.js
+++ script.js
@@ -411,3 +411,13 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
+});
